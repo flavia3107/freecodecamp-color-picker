cec9da0edb9454579216e6512ef05448c95068da
Flavia Kosti <flaviakosti@gmail.com>
2025-04-09 22:13:18 -0400

    Color Picker: Generate Random Color

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const selectedColor = document.querySelector('.selected-color');
 
 document.getElementById('colorInput').addEventListener('input', function () {
-	const color = this.value;
-	document.getElementById('hexValue').textContent = color;
-	document.getElementById('rgbValue').textContent = hexToRgb(color);
-	selectedColor.style.background = color;
+	refreshColorDetails(this.value);
 });
+
+document.querySelector('#generateRandomBtn').addEventListener('click', generateColor);
+
 function hexToRgb(hex) {
 	const r = parseInt(hex.slice(1, 3), 16);
 	const g = parseInt(hex.slice(3, 5), 16);
 	const b = parseInt(hex.slice(5, 7), 16);
 	return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	refreshColorDetails(color);
+}
+
+function refreshColorDetails(color) {
+	document.getElementById('hexValue').textContent = color;
+	document.getElementById('rgbValue').textContent = hexToRgb(color);
+	document.getElementById('colorInput').value = color;
+	selectedColor.style.background = color;
 }
